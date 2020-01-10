38087328ae2fbda8b740185a0ca054989f050b17
GhadaAboraya <ghada.aboraya55@gmail.com>
2020-01-11 00:23:04 +0200

    Updates

---
+++ precache-manifest.c20f6904d6d095e202a834029a9ca635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5557461bf3fb861987124ca1e52787f6",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "dad854f0195ef13297a9",
+    "url": "/Todo-App/static/css/main.bdb1e09a.chunk.css"
+  },
+  {
+    "revision": "82ff3437bfe7646c0e9f",
+    "url": "/Todo-App/static/js/2.6ac0b7b2.chunk.js"
+  },
+  {
+    "revision": "dad854f0195ef13297a9",
+    "url": "/Todo-App/static/js/main.3d0f4515.chunk.js"
+  },
+  {
+    "revision": "a9a1df87e8e4906d8452",
+    "url": "/Todo-App/static/js/runtime-main.b28af395.js"
+  }
+]);
